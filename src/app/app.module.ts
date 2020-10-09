96995f4e750828b6ed07c0a6d79c460935ca5361
Larissa Zamin da Costa <x376749@gruposantander.com>
2020-10-09 14:13:23 -0300

    Implementação das outras rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { AtendimentoComponent } from './atendimento/atendimento.component';
+import { HomeComponent } from './home/home.component';
+import { RelatorioComponent } from './relatorio/relatorio.component';
+import { UnidadeComponent } from './unidade/unidade.component';
 
 @NgModule({
-  declarations: [AppComponent, AtendimentoComponent],
+  declarations: [AppComponent, AtendimentoComponent, HomeComponent, RelatorioComponent, UnidadeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
